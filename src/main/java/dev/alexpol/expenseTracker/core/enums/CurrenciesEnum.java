ccc2319f03f76d09caff78602a9d4108ea624521
Alexandros Polymeros <alpolymer@gmail.com>
2025-09-01 17:46:52 +0300

    feat: add Budget class

---
+++ src/main/java/dev/alexpol/expenseTracker/core/enums/CurrenciesEnum.java
@@ -0,0 +1,7 @@
+package dev.alexpol.expenseTracker.core.enums;
+
+public enum CurrenciesEnum {
+    EUR,
+    USD,
+    GBP
+}
---
+++ src/main/java/dev/alexpol/expenseTracker/model/Budget.java
@@ -0,0 +1,28 @@
+package dev.alexpol.expenseTracker.model;
+
+import dev.alexpol.expenseTracker.core.enums.CurrenciesEnum;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class Budget extends AbstractEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Budget title is required")
+    @Size(max = 50, message = "Budget title  must not exceed 50 characters")
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private CurrenciesEnum currency;
+
+}
