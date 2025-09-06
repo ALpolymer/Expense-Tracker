5635ca5104fd77dfdd3272e146edb2481f853acd
Alexandros Polymeros <alpolymer@gmail.com>
2025-09-06 16:46:11 +0300

    feat: add UserRepositoryTest

---
+++ src/test/java/dev/alexpol/expenseTracker/repository/UserRepositoryTest.java
@@ -0,0 +1,23 @@
+package dev.alexpol.expenseTracker.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UserRepositoryTest {
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void existsByUsername() {
+    }
+
+    @Test
+    void existsByEmail() {
+    }
+}
